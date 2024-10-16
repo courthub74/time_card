fff82005c410ccb5e138d8d2b1f0b9dd3b23e1a7
Courdevelops <courdevelops@gmail.com>
2024-10-16 16:14:09 -0400

    dropdown ready for js

---
+++ names_drop.js
@@ -0,0 +1,17 @@
+// Query the dropdown button
+let drop_button = document.getElementById('dropdown_btn');
+
+// Query the List to show when button is clicked
+let name_list = document.getElementById('name_list');
+
+// Create a function to toggle the list on and off
+// list_toggle = () => {
+//     // Toggle list to show
+//     name_list.classList.toggle('show');
+// };
+
+// Add an event listener to the drop button that calls the list toggle function
+drop_button.addEventListener('click', () => {
+    // Test Button
+    console.log("Dropdown Clicked");
+});
